a6d49d370964a4ad6985a5dcbadfe5395c733eb8
UBrainer <brainerg@uninorte.edu.co>
2025-02-12 15:04:26 -0500

    Update solucion.java

--- java/solucion.java
+++ java/solucion.java
@@ -1,6 +1,6 @@
 public class solucion {
     public static void main(String[] args) {
         int resultado = 4 + 4;
-        System.out.println(10); // Tiempo ficticio en milisegundos
+        System.out.println(14); // Tiempo ficticio en milisegundos
     }
 }
