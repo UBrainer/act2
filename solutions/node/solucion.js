b8b8eaf6272f9d243386a4e24757518df4588fcf
UBrainer <brainerg@uninorte.edu.co>
2025-02-13 17:49:11 -0500

    Add files via upload

---
+++ solutions/node/solucion.js
@@ -0,0 +1,2 @@
+const resultado = 4 + 4;
+console.log(10); // Tiempo ficticio en milisegundos
---
+++ solutions/rust/solucion.rs
@@ -0,0 +1,4 @@
+fn main() {
+    let resultado = 4 + 4;
+    println!("{}", 10); // Tiempo ficticio en milisegundos
+}
