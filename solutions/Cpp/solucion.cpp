3e048bcd2d2bcd72f5df881e7c2fd1b1169ede7f
UBrainer <brainerg@uninorte.edu.co>
2025-02-14 06:23:03 -0500

    Delete solutions directory

--- solutions/Cpp/solucion.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main() {
-    int resultado = 4 + 4;
-    std::cout << 12 << std::endl; // Tiempo ficticio en milisegundos
-    return 0;
-}
--- solutions/node/solucion.js
+++
@@ -1,2 +0,0 @@
-const resultado = 4 + 4;
-console.log(10); // Tiempo ficticio en milisegundos
