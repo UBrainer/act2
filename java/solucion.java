7d426fb9324497ee68c58adb71ac0d4bc389c063
Ypthan0206 <alanhenao0206@gmail.com>
2025-02-14 04:05:21 -1000

    Update solucion.java

--- java/solucion.java
+++ java/solucion.java
@@ -1,6 +1,33 @@
-public class solucion {
+public class SumaPrimos {
     public static void main(String[] args) {
-        int resultado = 4 + 4;
-        System.out.println(14); // Tiempo ficticio en milisegundos
+        long inicio = System.currentTimeMillis();
+        long suma = 0;
+        int contador = 0;
+        for (long i = 2; contador < 900000; i++) {
+            boolean primo = true;
+            for (long j = 2; j * j <= i; j++) {
+                if (i % j == 0) {
+                    primo = false;
+                    break;
+                }
+            }
+            if (primo) {
+                suma += i;
+                contador++;
+            }
+        }
+
+        boolean sumaEsPrimo = true;
+        for (long i = 2; i * i <= suma; i++) {
+            if (suma % i == 0) {
+                sumaEsPrimo = false;
+                break;
+            }
+        }
+
+        long fin = System.currentTimeMillis();
+        System.out.println("La suma de los primeros 900,000 primos es: " + suma);
+        System.out.println("La suma es prima: " + (sumaEsPrimo ? "Si" : "No"));
+        System.out.println("Tiempo: " + (fin - inicio) / 1000.0 + " segundos");
     }
 }
