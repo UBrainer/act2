c08275f2517d68ac4e4a0441df4eb2f40185492a
Ypthan0206 <alanhenao0206@gmail.com>
2025-02-14 12:26:20 -0500

    Update solucion.cpp

--- cpp/solucion.cpp
+++ cpp/solucion.cpp
@@ -17,7 +17,7 @@ int main() {
 
     long long suma = 0;
     int contador = 0;
-    for (long long i = 2; contador < 900000; ++i) {
+    for (long long i = 2; contador < 9000; ++i) {
         bool primo = true;
         for (long long j = 2; j * j <= i; ++j) {
             if (i % j == 0) {
@@ -31,21 +31,10 @@ int main() {
         }
     }
 
-    bool sumaEsPrimo = true;
-    for (long long i = 2; i * i <= suma; ++i) {
-        if (suma % i == 0) {
-            sumaEsPrimo = false;
-            break;
-        }
-    }
-
     auto fin = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> duracion = fin - inicio;
 
-    std::cout << "La suma de los primeros 900,000 primos es: " << suma << "\n";
-    std::cout << "La suma es prima: " << (sumaEsPrimo ? "Si" : "No") << "\n";
-    std::cout << "Tiempo: " << duracion.count() << " segundos\n";
+    std::cout << duracion.count() << "\n";
     
     return 0;
 }
-
