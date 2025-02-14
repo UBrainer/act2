f046bc491f304e19a1cc506e2652ba6923c45a7d
UBrainer <brainerg@uninorte.edu.co>
2025-02-14 06:30:21 -0500

    Reinicio

---
+++ node/solucion.js
@@ -0,0 +1,2 @@
+const resultado = 4 + 4;
+console.log(20); // Tiempo ficticio en milisegundos
---
+++ python/solucion.py
@@ -0,0 +1,4 @@
+# Realiza la suma 4 + 4 y muestra solo el tiempo ficticio
+resultado = 4 + 4
+print(11)  # Tiempo ficticio en milisegundos
+
